8c281d9bcdf808757d7ef0aee58ebdc7b21aaa43
Eli Goldner <egoldner@brandeis.edu>
2020-07-23 14:55:44 -0400

    Changes to be committed: 	new file:   progress-and-todo.org 	modified:   rule-out.py

--- rule-out.py
+++ rule-out.py
@@ -4,9 +4,7 @@ import re
 import ast
 from stanza.server import CoreNLPClient
 from bllipparser import RerankingParser
-from nltk.tree import Tree
-from nltk.corpus import wordnet as wn
-from nltk.treeprettyprinter import TreePrettyPrinter
+from nltk.tree import ParentedTree
 from pattern.en import conjugate
 from pattern.en import INFINITIVE, PRESENT, SG, SUBJUNCTIVE, PAST, PARTICIPLE
 
@@ -18,6 +16,19 @@ client = CoreNLPClient(annotators=['parse'], timeout=30000, memory='8G')
 auxes = {'have', 'be', 'do', 'would', 'could',  'might'}
 clause_levels = {'S', 'SBAR', 'SBARQ', 'SINV', 'SQ'}
 root_levels = {'ROOT', 'S1'}
+punct = {'.', '.', '?'}
+
+# This guards against multiple identical
+# subtrees appearing in the full tree barring
+# some bizarre literary case like
+# "We saw the cars go by and we saw the cars go by"
+def peq(p1, p2):
+    teq = p1 == p2
+    place_eq = p1.parent() == p2.parent()
+    return teq and place_eq
+
+def children(ptree):
+    return [stree for stree in ptree]
 
 def non_finite(v):
     return {conjugate(v, INFINITIVE),
@@ -28,14 +39,17 @@ def is_non_finite(v):
     return v in non_finite(v)
 
 def is_aux(v):
-    return (v == 'MD') or (conjugate(v, INFINITIVE) in auxes) 
+    return (v.label() == 'MD') or (conjugate(children(v)[0], INFINITIVE) in auxes) 
 
 def is_non_aux(v):
-    return v[:2] == 'VB'
+    return v.label()[:2] == 'VB'
 
-# returns the nearest upper embedded clause 
+# returns the nearest upper embedded clause
+# (determines if the immediate clause is embedded) 
 def sup_embedded(ptree):
     if ptree.label() in root_levels.union(clause_levels)):
+        # this happens often with coordinated conjunctions and does not
+        # constitute embedding
         if (ptree.parent().label() in root_levels.union(clause_levels)):
             return None
         else:
@@ -43,18 +57,26 @@ def sup_embedded(ptree):
     else:
         sup_embedded(ptree.parent()) 
 
-# returns the nearest lower embedded clause
-def inf_embedded(ptree):
-    if ptree.label() in root_levels.union(clause_levels)):
-        for i, stree in enumerate(ptree):
-            
-def is_simple(ptree):
-    isroot =  (ptree.label() == 'ROOT' or ptree.label() == 'S1') 
-    no_embedded = all[(not contains_embedded(stree)) for i, stree in enumerate(ptree)]
-    return isroot and no_embedded
+# returns the nearest lower embedded clauses
+def inf_embedded(ptree, embedded = []):
+    if children(ptree) == ptree.leaves():
+        return None
+    if ptree.label() not in root_levels.union(clause_levels)):
+        for stree in ptree:
+            if stree.label() in clause_levels:
+                embedded.append(stree)
+            else:
+                inf_embedded(stree, embedded)
+    return embedded
+
+def is_simple(ptree): 
+    return all[(not contains_embedded(stree)) for stree in ptree.root()]
 
 def clause_overt_v_head(ptree):
-    pass
+    head_tag = children(ptree)[0]
+    if ptree.label() == 'VP':
+        
+            
 
 def rule_out(ptree):
     pass
@@ -66,21 +88,8 @@ def overt_not_aux(ptree):
     
 
 def main():
-    f_name = sys.argv[1]
-    f = open(f_name, 'r')
-    for line in f:
-        temp = line.split(':')
-        index = temp[0]
-        text = temp[1]
-        ann = client.annotate(text, output_format='json')
-        s_parse_tree = ann['sentences'][0]['parse']
-        s_final = list2ptree(s_parse_tree)
-        b_parse_tree = rrp.simple_parse(text)
-        b_final = list2ptree(b_parse_tree)
-        print("index : {0} \n\n Stanford tree: \n {1} \n\n Charniak tree: \n {1}".format(index,
-                                                                                         s_final,
-                                                                                         b_final))
-    
+    pass
+
 if __name__ == "__main__":
     main()
 
